ca52b3e98ed36bfdf7fdab2581f0e52441651650
digmouse233 <digmouse233@gmail.com>
2022-09-14 21:51:00 -0400

    20220914: Java solution

--- problems/2022_09_14/0019/Solution.java
+++ problems/2022_09_14/0019/Solution.java
@@ -6,6 +6,28 @@
 
 public class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(-1, head);
 
+        ListNode slow = dummy, fast = dummy;
+
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+        
     }
 }
+
+        
+        
+
+        
+        
--- problems/2022_09_14/0143/Solution.java
+++ problems/2022_09_14/0143/Solution.java
@@ -6,6 +6,37 @@
 
 public class Solution {
     public void reorderList(ListNode head) {
+        if (head == null) {
+            return;
+        }
 
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode prev = null, curr = slow;
+        while (curr != null) {
+            ListNode tmp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = tmp;
+        }
+
+        ListNode first = head, second = prev;
+        while (second.next != null) {
+            ListNode tmp = first.next;
+            first.next = second;
+            first = tmp;
+
+            tmp = second.next;
+            second.next = first;
+            second = tmp;
+        }
+        
     }
 }
+
+        
+        
